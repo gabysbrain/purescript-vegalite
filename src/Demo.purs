4ce6279e363e1822919bd974b3b38d60e0273f22
Thomas Torsney-Weir <torsneyt@gmail.com>
2018-06-27 10:11:52 +0200

    demo functionality

---
+++ src/Demo.purs
@@ -0,0 +1,19 @@
+module VegaLiteDemo where
+
+import MyPrelude
+import Control.Monad.Eff (Eff)
+import VegaLite
+
+bg :: View
+bg = Simple $ setData d $ viewPanel Bar encs
+  where
+  d = UrlData {url: "https://vega.github.io/vega-lite/data/seattle-weather.csv", format: Nothing}
+  encs = 
+    markX (posTimeUnit Month $ posField "date"$ position Ordinal) $
+    markY (posAggregate Mean $ posField "precipitation" $ position Quantitative) $
+    encoding
+
+main :: Eff () Unit
+main = pure $ embedView "#vis" bg
+  
+
